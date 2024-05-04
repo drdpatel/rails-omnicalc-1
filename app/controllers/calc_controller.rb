1d49f6f0c908ecd24da683edce279e5ca11912b0
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-04 16:04:48 +0000

    results routes and views defineded

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -5,9 +5,23 @@ class CalcController < ApplicationController
     render({ :template => "path/home" })
   end
 
+  def get_square_results
+
+    @the_num = params.fetch("square_with_form").to_f
+    @the_result = params.fetch("square_with_form").to_f ** 2
+
+    render({ :template => "path/square_results"})
+
+  end
+
   def get_square_root
 
     render({ :template => "path/square_root" })
+  end
+
+  def get_square_root_results
+
+    render({ :template => "path/square_root_results" })
   end
 
   def get_payment
@@ -15,8 +29,18 @@ class CalcController < ApplicationController
     render({ :template => "path/payment" })
   end
 
+  def get_payment_results
+
+    render({ :template => "path/payment_results" })
+  end
+
   def get_random
 
     render({ :template => "path/random" })
+  end
+
+  def get_random_results
+
+    render({ :template => "path/random_results" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   
 
   get("/", { :controller => "calc", :action => "get_home" })
+
   get("/square/new/", { :controller => "calc", :action => "get_home" })
+  get("/square/results/", { :controller => "calc", :action => "get_square_results" })
+  
   get("/square_root/new/", { :controller => "calc", :action => "get_square_root" })
+  get("/square_root/results/", { :controller => "calc", :action => "get_square_root_results" })
+
   get("/payment/new/", { :controller => "calc", :action => "get_payment" })
+  get("/payment/results/", { :controller => "calc", :action => "get_payment_results" })
+  
   get("/random/new/", { :controller => "calc", :action => "get_random" })
+  get("/random/results/", { :controller => "calc", :action => "get_random_results" })
 end
